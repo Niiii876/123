896859b4793adefdce3d9ea1808f7f88ea02b0a4
Niiii876 <2295769215@qq.com>
2026-05-14 11:22:02 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -4,6 +4,6 @@ public class test {
     public static void main(String[] args) {
         id=10+1;
         System.out.println("id");
-        System.out.println("123456789");
+        System.out.println("12");
     }
 }
